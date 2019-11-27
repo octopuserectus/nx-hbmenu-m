68f7701dd55654ed302f690f4e6ad2e721f63377
octopuserectus <james_gary@my.cuesta.edu>
2019-11-27 17:21:43 +0100

    Accept inputs from all players, not 0 and 1

--- nx_main/main.c
+++ nx_main/main.c
@@ -197,7 +197,12 @@ int main(int argc, char **argv)
 bool menuUpdate(void) {
     bool exitflag = 0;
     menu_s* menu = menuGetCurrent();
-    u32 down = hidKeysDown(CONTROLLER_P1_AUTO);
+    u64 down = 0;
+
+    for (int controller = 0; controller < 10; controller++) {
+        down |= hidKeysDown((HidControllerID) controller);
+    }
+    
     handleTouch(menu);
 
     if (down & KEY_Y)
